37027fdbba2ae19083d57ca16629e6cc60213d67
Daniel Moreno <daniel.moreno@creativaconsultores.com>
2019-07-02 16:47:10 -0600

    Added Crud view

---
+++ EfProject/Controllers/CrudController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace EfProject.Controllers
+{
+    public class CrudController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
